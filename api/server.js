70f1ebba39a5bef9bdba1e63dea0cc2b45ad8bbe
oogi <no-reply>
2021-11-30 00:25:47 -0500

    Setup Prettier

--- api/server.js
+++ api/server.js
@@ -1,26 +1,30 @@
-const express = require('express'),
-	path = require('path'),
-	bodyParser = require('body-parser'),
-	cors = require('cors'),
-	mongoose = require('mongoose'),
-	config = require('./DB');
+const express = require("express"),
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  cors = require("cors"),
+  mongoose = require("mongoose"),
+  config = require("./DB");
 
-const userRoute = require('./routes/user.route');
+const userRoute = require("./routes/user.route");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true}).then(
-	() => { console.log("Successfully connected to Database")},
-	err => { console.log('Cannot connect to Database ' + err) }
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Successfully connected to Database");
+  },
+  (err) => {
+    console.log("Cannot connect to Database " + err);
+  }
 );
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/users',userRoute);
+app.use("/users", userRoute);
 let port = process.env.PORT || 4000;
 
-const server = app.listen(port, ()=> {
-	console.log("Listening on port " + port);
+const server = app.listen(port, () => {
+  console.log("Listening on port " + port);
 });
 
 module.exports = server;
